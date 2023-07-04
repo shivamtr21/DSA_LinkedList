a63c385ef0518247632b2834a1192d096635905d
shivamtr21 <shivamtripathi26297@gmail.com>
2023-07-04 09:41:56 +0530

    [ shivam tripathi ] : Added Linked list Problem UC1

---
+++ DataStructureDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+namespace DataStructureDemo
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+
+            list.Display();
+
+           
+        }
+    }
+
+}
